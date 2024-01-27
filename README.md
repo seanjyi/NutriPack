a25dcac548a375210b654128a923116e5925b1a2
seanjyi <seanjyi01@gmail.com>
2024-01-27 12:31:47 -0500

    updated README files, added endpoints

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-To run
+To run after initial
+> docker-compose up
+
+To run after Docker changes
 > docker-compose up --build --force-recreate
--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,26 @@
-from flask import Flask
+import food
+from flask import Flask, request
  
 server = Flask(__name__)
 
-@server.route('/')
-def hello_world():
-    return 'Hello World'
+# temp dict variable; k: user, v: array of meals 
+database = {}
+
+# USER ENDPOINTS
+@server.route('/user/meal', methods=["GET"])
+def get_user_meals():
+    return ""
+
+@server.route('/user/meal', methods=["POST"])
+def add_user_meal():
+    new_meal = request.get_json()
+    return search_req
+
+# SEARCH ENDPOINTS
+@server.route('/search', methods=["POST"])
+def food_search():
+    search_req = request.get_json()
+    return search_req
 
 if __name__ == '__main__':
     server.run()
