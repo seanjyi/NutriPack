1a8c33f5aef398d3cb63b6d392575f12fa089dff
Abel Lu <aluprof4@gmail.com>
2024-01-28 02:02:14 -0500

    removed navbar from sustainability dashboard

--- frontend/src/app/stats/page.tsx
+++ frontend/src/app/stats/page.tsx
@@ -35,16 +35,16 @@ export default function DashboardPage() {
   return (
     <>
       <div className="flex flex-col">
-        <div className="border-b">
+        {/* <div className="border-b">
           <div className="flex h-16 items-center px-4">
-            {/* <TeamSwitcher /> */}
+            <TeamSwitcher />
             <MainNav className="mx-6" />
             <div className="ml-auto flex items-center space-x-4">
               <Search />
               <UserNav />
             </div>
           </div>
-        </div>
+        </div> */}
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">
